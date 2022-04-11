ec468c53527814d6caebffc3d5b929040bb94da9
Ashfaaq Rifath <ashfaaq.rifath2@gmail.com>
2022-04-11 12:30:55 +0530

    uploaded main file.

---
+++ YTdownloader.py
@@ -0,0 +1,32 @@
+from pytube import YouTube
+from moviepy.editor import VideoFileClip
+import colorama
+from colorama import Fore, Back
+colorama.init(autoreset=True)
+
+def mp3Convert(filename):
+    video = VideoFileClip(filename)
+    video.audio.write_audiofile(filename[:-4] + ".mp3")
+    video.close()
+
+
+SAVE_PATH = "G:/My Drive/YT downloader"
+
+getLink = input(Fore.GREEN + "Enter YouTube link: ")
+usr_extention = input(Fore.MAGENTA + "Do you want to convert to mp3 format: ")
+dwnld_file = YouTube(getLink)
+
+print(Fore.BLACK + Back.YELLOW + f" Video Title: {dwnld_file.title} ")
+
+for stream in dwnld_file.streams:
+    print(Fore.CYAN + str(stream))
+
+dwnld_file = dwnld_file.streams.get_lowest_resolution()
+test = dwnld_file.download(SAVE_PATH)
+
+if usr_extention.lower() == "y":
+    mp3Convert(test)
+    print(Fore.BLACK + Back.GREEN + " Converted to mp3 ")
+
+else:
+    print(Fore.BLACK + Back.GREEN + " Completed ")
