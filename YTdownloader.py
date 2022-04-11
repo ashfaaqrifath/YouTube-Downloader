436efab7263cdd26a858089ba2d759a902602cb4
Ashfaaq Rifath <ashfaaq.rifath2@gmail.com>
2022-04-11 18:07:52 +0530

    Update YTdownloader.py

--- YTdownloader.py
+++ YTdownloader.py
@@ -13,7 +13,7 @@ def mp3Convert(filename):
 SAVE_PATH = "file path goes here."
 
 getLink = input(Fore.GREEN + "Enter YouTube link: ")
-usr_extention = input(Fore.MAGENTA + "Do you want to convert to mp3 format: ")
+usr_extention = input(Fore.MAGENTA + "Do you want to convert to mp3 format (y/n): ")
 dwnld_file = YouTube(getLink)
 
 print(Fore.BLACK + Back.YELLOW + f" Video Title: {dwnld_file.title} ")
